1e7195389d61134bdec5dd0bc8d24b5f46f0dd6e
mukulBillore <mukulbillore88@gmail.com>
2021-12-26 14:15:57 +0530

    Added : added the functionality to find the max among three using user input

--- src/com/bridgeLabs/MaxFinder.java
+++ src/com/bridgeLabs/MaxFinder.java
@@ -1,5 +1,40 @@
 package com.bridgeLabs;
 
+import java.util.Scanner;
+
 public class MaxFinder {
+
+	static Integer firstNo;
+	static Integer secondNo;
+	static Integer thirdNo;
+
+	public static void testMaximum(Integer a, Integer b, Integer c) {
+		//let max is a 
+		Integer max = a;
+		if (b.compareTo(max) > 0) {
+			max = b;
+		}
+		if (c.compareTo(max) > 0) {
+			max = c;
+		}
+		System.out.println(max + " is maximum number");
+	}
+
+	public static void getUserInput() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number one");
+		firstNo = sc.nextInt();
+		System.out.println("Enter Number two");
+		secondNo = sc.nextInt();
+		System.out.println("Enter Number three");
+		thirdNo = sc.nextInt();
+	}
+
+	public static void main(String[] args) {
+
+		getUserInput();
+		testMaximum(firstNo, secondNo, thirdNo);
+
+	}
 
 }

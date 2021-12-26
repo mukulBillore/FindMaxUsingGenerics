cb01bd8234b67565539c99a3f47bf9adc768c330
mukulBillore <mukulbillore88@gmail.com>
2021-12-26 14:58:21 +0530

    Added : added the functionality to find the max among three using user float input

--- src/com/bridgeLabs/MaxFinder.java
+++ src/com/bridgeLabs/MaxFinder.java
@@ -7,7 +7,21 @@ public class MaxFinder {
 	static Integer firstNo;
 	static Integer secondNo;
 	static Integer thirdNo;
-
+	static Float firstNox;
+	static Float secondNoy;
+	static Float thirdNoz;
+	private static void testMaximum(Float a, Float b, Float c) {
+		
+		//let max is a 
+				Float max = a;
+				if (b.compareTo(max) > 0) {
+					max = b;
+				}
+				if (c.compareTo(max) > 0) {
+					max = c;
+				}
+				System.out.println(max + " is maximum number");
+			}
 	public static void testMaximum(Integer a, Integer b, Integer c) {
 		//let max is a 
 		Integer max = a;
@@ -19,8 +33,16 @@ public class MaxFinder {
 		}
 		System.out.println(max + " is maximum number");
 	}
-
-	public static void getUserInput() {
+	public static void getFloatUserInput() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number one");
+		firstNox = sc.nextFloat();
+		System.out.println("Enter Number two");
+		secondNoy = sc.nextFloat();
+		System.out.println("Enter Number three");
+		thirdNoz = sc.nextFloat();
+	}
+	public static void getIntUserInput() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Number one");
 		firstNo = sc.nextInt();
@@ -32,9 +54,12 @@ public class MaxFinder {
 
 	public static void main(String[] args) {
 
-		getUserInput();
+		getIntUserInput();
 		testMaximum(firstNo, secondNo, thirdNo);
-
+		getFloatUserInput();
+		testMaximum(firstNox, secondNoy, thirdNoz);
+		
 	}
+		}
 
-}
+
